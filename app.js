8a35625d87af1e3f0b3f51065f0af240380de153
siddhant0321 <84197714+siddhant0321@users.noreply.github.com>
2022-03-31 22:04:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('#search');
+const res = document.querySelector('#tableResult');
+var upd;
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    if(upd){
+        clearTimeout(upd);
+    }
+    const ctype = form.elements.coinType.value;
+    fetchPrice(ctype);
+});
+
+// const fetchPrice = async(ctype)=>{
+//   const r = await axios.get(`https://api.coinstats.app/public/v1/${ctype}?currency=INR`);
+//   console.log(r.data.coin.price);
+//     const base = r.data.coin.name;
+//     const rank = r.data.coin.rank;
+//     const price = r.data.coin.price;
+//     const volume = r.data.coin.volume;
+//     const change = r.data.coin.priceChange1h;
+//     const targer ='USD';
+
+const fetchPrice= async(ctype) =>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+    console.log(r.data.coin.price);
+     const price = r.data.coin.price;
+     const volume  = r.data.coin.volume;
+     const change = r.data.coin.priceChange1d;
+     const base = r.data.coin.name;
+     const rank = r.data.coin.rank;
+     const target = 'INR';
+
+
+    res.innerHTML =`<tr style="  border:1px solid black;
+      font-size:24px;
+      width:6px;
+      background-color:lightblue;">
+<td>
+    Property
+</td>
+<td>Value</td>
+</tr>
+<tr>
+<td>
+    ${base}
+</td>
+<td>${price} ${target}</td>
+</tr>
+<tr>
+<td>
+    Rank
+</td>
+<td>${rank}</td>
+</tr>
+<tr>
+<td>
+    Volume
+</td>
+<td>${volume}</td>
+</tr>
+<tr>
+<td>
+    Change
+</td>
+<td>${change}</td>
+</tr>`
+
+upd = setTimeout(()=>fetchPrice(ctype),10000);
+}
